ad92a693e57e71e2219046e19a5b5edab6091678
Wanderson Alves <wandersonalvesllima1@icloud.com>
2024-09-12 20:29:27 -0300

    Adicionando os arquivos da pratica4

---
+++ Praticas/Pratica4/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+   <div className="card">
+     <Header/>
+     <p>Este é um parágrafo em JSX!</p>
+   </div>
+  );
+}
+
+export default App;
---
+++ Praticas/Pratica4/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return (
+     <header>
+       <h1>Minha Aplicação React</h1>
+     </header>
+    );
+  }
+  
+
+export default Header;
